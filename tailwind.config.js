44667f6884796d014c93ab3f97b73d05591942ba
Khaira Farzana <khaira@pensil.com>
2023-06-09 19:19:21 +0700

    This is my Tic-Tac-Toe

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+	content: ["./index.html", "./src/**/*.{js,ts,tsx}"],
+	theme: {
+		extend: {
+            fontFamily: {
+                display:['Lobster', ...defaultTheme.fontFamily.sans]
+            }
+        },
+	},
+	plugins: [],
+};
