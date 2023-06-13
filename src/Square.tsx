53cf3e8813428f7aa9db40adfa3a9847f98efaf9
Khaira Farzana <khaira@pensil.com>
2023-06-13 19:54:06 +0700

    This is my Tic-Tac-Toe

--- src/Square.tsx
+++ src/Square.tsx
@@ -11,7 +11,7 @@ function Square({index, onClick,player}: Props){
   return(
 <div	
     data-cell-index={index}
-     className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
+     className={`h-36 bg-cover border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
      {...{onClick}} >
         
     <span 
